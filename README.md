aae39fbc63a0c5f37fa47840d81c04f9a871e785
Ed Scheinerman <ers@jhu.edu>
2023-06-15 07:32:01 -0400

    Complete makeover

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # `McConstant`
 What is the largest number of McNuggets that cannot be purchased?
 
+
 ## Buying McNuggets 
 
 Assuming that McDonalds sells Chicken McNuggets in portions of size 6, 9, or 20
@@ -29,42 +30,26 @@ julia> mc_constant()
 
 This means that it is impossible to purchase exactly 43 McNuggets, but for any value of `n` greater than 43, it is possible to purchase exactly `n` McNuggets. 
 
-## How to order
 
-The function `mc_order` returns instructions on how to order `n` McNuggets.
+## Generalized McNuggets
+
+The functions `mc_possible` and `mc_constant` may be fed alternative portion sizes other than the default 6, 9, and 20.
+* `mc_possible(n, nugs...)` determines if it is possible to purchase `n` McNuggets with the portion sizes are given in the trailing arguments.
+* `mc_constant(nugs...)` determines the McConstant given the portion sizes in the arguments. 
+
+### Example
 ```
-julia> mc_order(44)
-Order 1 six-piece, 2 nine-piece, and 1 twenty-piece
+julia> mc_possible(20,5,9)
+true
 
-julia> mc_order(43)
-It is not possible to order 43 McNuggets
+julia> mc_possible(21,5,9)
+false
 
-julia> mc_order(888)
-Order 2 six-piece, 4 nine-piece, and 42 twenty-piece
-```
-
-The `mc_order` function invokes the `mc_solver` function that returns a tuple `(a,b,c)` showing how many
-of each size to order. That is, `6a + 9b + 20c == n`. Throws an error if `n` is not orderable. 
-```
-julia> mc_solver(44)
-(1, 2, 1)
-
-julia> mc_solver(43)
-ERROR: Cannot order 43 McNuggets
+julia> mc_constant(5,9)
+31
 ```
 
+If McNuggets are sold in packages of 5 and 9, then it is possible to purchase 20 McNuggets, but it is not possible to purchase 21.
 
-
-## Why?
-
-Because I was bored. Also, I'm not sure that six, nine, and twenty piece servings are
-correct. I think this was correct at some point, but maybe not any more?
-
-## Bugs
-
-This code can't help you if you want to order millions of McNuggets:
-```
-julia> mc_order(1_000_000)
-ERROR: StackOverflowError:
-```
-But in this case, you have a more serious problem than a stack overflow. 
+Further, it is impossible to purchase 31 McNuggets, but it is possible to purchase
+any amount greater than 31. 
--- src/McConstant.jl
+++ src/McConstant.jl
@@ -1,44 +1,75 @@
 module McConstant
-using Memoize
-export mc_possible, mc_constant, mc_solver, mc_order
+export mc_possible, mc_constant
+
+
+function nug_check(nugs...)::Nothing
+    if length(nugs) == 0
+        error("No nugget sizes specified")
+    end
+
+    if minimum(nugs) <= 0
+        error("Nugget sizes must be positive")
+    end
+
+    nothing
+end
+
 
 """
-    mc_possible(n::Int)::Bool
+    mc_possible(n::Int, nugs...)::Bool
 
-Determine if it is possible to purchase `n` McNuggets assuming the
-sizes available are 6, 9, or 20. Return `true` is possible.
+Determine if it is possible to purchase `n` McNuggets given
+the serving sizes specified in `nugs`.
 
 # Example
 ```
-julia> mc_possible(19)
+julia> mc_possible(43,6,9,20)
 false
 
-julia> mc_possible(21)
+julia> mc_possible(44,6,9,20)
 true
 ```
 """
-@memoize function mc_possible(n::Int)::Bool
+function mc_possible(n::Int, nugs...)::Bool
+
+
+    if length(nugs) != length(unique(nugs))
+        @warn "Repeated nugget size: $nugs"
+    end
+
+
+    nug_check(nugs...)
+
+    sorted_nugs = sort(collect(nugs), rev = true)
+    nugs = tuple(sorted_nugs...)
+
+    mc_possible_work(n, nugs...)
+end
+
+mc_possible(n::Int) = mc_possible(n, 6, 9, 20)
+
+function mc_possible_work(n::Int, nugs...)::Bool
     if n == 0
         return true
     end
-    if n < 6
+    if n < nugs[end]
         return false
     end
 
-    if mc_possible(n - 6) || mc_possible(n - 9) || mc_possible(n - 20)
-        return true
+    for s ∈ nugs
+        if mc_possible_work(n - s, nugs...)
+            return true
+        end
     end
+
     return false
 end
 
-"""
-    streak(n::Int)::Bool
 
-See if there are 6 consecutive `true` returns from `mc_possible` for 
-values from `n` to `n+5`.
-"""
-function streak(n::Int)::Bool
-    list = [mc_possible(k) for k = n:n+5]
+
+function streak(n::Int, nugs...)::Bool
+    s_min = minimum(nugs)
+    list = [mc_possible(k, nugs...) for k = n:n+s_min-1]
     return all(list)
 end
 
@@ -48,10 +79,20 @@ end
 Calculate the McConstant, i.e., the largest number of Chicken McNuggets
 that one cannot purchase.
 """
-function mc_constant()::Int
-    n = 20
+function mc_constant(nugs...)::Int
+
+    d = gcd(nugs...)
+    if d > 1
+        error("Nugget sizes have a common divisor $d; gcd should be 1")
+    end
+
+    if minimum(nugs) == 1
+        error("Nugget portion sizes must all be greater than 1")
+    end
+
+    n = 1
     while true
-        if !mc_possible(n) && streak(n + 1)
+        if !mc_possible(n, nugs...) && streak(n + 1, nugs...)
             return n
         end
         n += 1
@@ -59,67 +100,6 @@ function mc_constant()::Int
     0  # this shouldn't happen
 end
 
-"""
-    mc_solver(n::Int)::Tuple{Int,Int,Int}
+mc_constant() = mc_constant(6, 9, 20)
 
-Helper function used by `mc_order`. Returns a tuple `(a,b,c)`
-that gives the number of six-, nine-, and twenty-piece 
-portions to order to get `n` McNuggets. That is,
-`6a+9b+20c == n`. 
-
-Throws an error if no order is possible. 
-
-# Example
-```
-julia> mc_solver(44)
-(1, 2, 1)
-```
-"""
-@memoize function mc_solver(n::Int)::Tuple{Int,Int,Int}
-    if !mc_possible(n)
-        error("Cannot order $n McNuggets")
-    end
-    if n == 0
-        return 0, 0, 0
-    end
-
-    if mc_possible(n - 20)
-        a, b, c = mc_solver(n - 20)
-        return a, b, c + 1
-    end
-
-    if mc_possible(n - 9)
-        a, b, c = mc_solver(n - 9)
-        return a, b + 1, c
-    end
-
-    if mc_possible(n - 6)
-        a, b, c = mc_solver(n - 6)
-        return a + 1, b, c
-    end
-
-    error("This can't happen")
-end
-
-"""
-    mc_order(n::Int)
-
-Explain how to purchase `n` McMuffins.
-
-# Example
-```
-julia> mc_order(44)
-Order 1 six-piece, 2 nine-piece, and 1 twenty-piece
-```
-"""
-function mc_order(n::Int)
-    if !mc_possible(n)
-        println("It is not possible to order $n McNuggets")
-        return
-    end
-
-    a, b, c = mc_solver(n)
-    println("Order $a six-piece, $b nine-piece, and $c twenty-piece")
-end
-
-end # module McConstant
+end #end of module
